658eaf38f6658bcc94f75a914e7d4b22f21e8ad7
Jackson Morawski <jaxmorawski@gmail.com>
2023-10-01 18:27:24 -0500

    Update with player table

--- src/screens/player_table_screen.py
+++ src/screens/player_table_screen.py
@@ -11,6 +11,7 @@ from kivy.uix.screenmanager import Screen
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.button import Button
+from supabase import create_client
 from .. import common
 
 class PlayerTableScreen(Screen):
@@ -30,6 +31,9 @@ class PlayerTableScreen(Screen):
         data_button = Button(text='Go Back', font_size=24)
         data_button.bind(on_press=self.switch_to_player_entry)
         root.add_widget(data_button)
+
+        #add the widget with the table
+
     
         self.add_widget(root)
 
@@ -39,14 +43,11 @@ class PlayerTableScreen(Screen):
         """
         self.laser_tag_system = system
 
-    def get_data(self, instance):
-        self.data = self.laser_tag_system.supabase.get_all_players()
-
     def switch_to_player_entry(self, delta_time):
         """
         The callback for the on_enter method which switches the screen to the player entry screen
         """
         self.laser_tag_system.switch_screen(common.PLAYER_ENTRY_SCREEN)
 
-    def get_data(self, instance):
-        self.data = self.laser_tag_system.supabase.get_all_players()
+    def get_data(self):
+        self.data = self.laser_tag_system.Supabase.get_all_players()
