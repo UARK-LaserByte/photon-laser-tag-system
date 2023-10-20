31f22786207890ce5db1c2f22658d1a9f89032cd
Eric-y-Lee <135090112+Eric-y-Lee@users.noreply.github.com>
2023-10-20 12:23:55 -0500

    Update player_action_screen.py

--- src/screens/player_action_screen.py
+++ src/screens/player_action_screen.py
@@ -3,8 +3,8 @@ src/screens/player_action_screen.py
 
 See description below.
 
-by Alex Prosser
-10/10/2023
+by Alex Prosser, Eric lee
+10/20/2023
 """
 
 import time
@@ -13,6 +13,8 @@ from kivy.clock import Clock
 from kivy.uix.screenmanager import Screen
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
+from kivy.uix.scrollview import ScrollView
+from kivy.uix.gridlayout import GridLayout
 from .. import common
 from typing import TYPE_CHECKING
 
@@ -38,7 +40,45 @@ class PlayerActionScreen(Screen):
         # create the root UI and add label for now
         root = BoxLayout(orientation='vertical')
         root.add_widget(Label(text='Player Action Screen'))
+
+        ####
+
+        self.row_count = 10
+
+        teams_layout = GridLayout(cols=2, size_hint=(1, 0.05))
+        red_team_label = Label(text="Red Team", size_hint=(1, 0.05))
+        green_team_label = Label(text="Green Team", size_hint=(1, 0.05))
+        teams_layout.add_widget(red_team_label)
+        teams_layout.add_widget(green_team_label)
+
+        names_scores_layout = GridLayout(cols=4, rows= self.row_count, size_hint=(1, 0.6))
+
+        for i in range(self.row_count):
+            red_name_label = Label(text= self.laser_tag_system.players[common.RED_TEAM][i], size_hint_x=0.2)
+            red_score_label = Label(text=f"Score: 0", size_hint_x=0.1)
+            green_name_label = Label(text=self.laser_tag_system.players[common.GREEN_TEAM][i], size_hint_x=0.2)
+            green_score_label = Label(text=f"Score: 0", size_hint_x=0.1)
+            
+            names_scores_layout.add_widget(red_name_label)
+            names_scores_layout.add_widget(red_score_label)
+            names_scores_layout.add_widget(green_name_label)
+            names_scores_layout.add_widget(green_score_label)
+        
+
         self.add_widget(root)
+
+        # Create a ScrollView for the chat log (at the bottom)
+        chat_scrollview = ScrollView(size_hint=(1, 0.20))
+        chat_logs = TextInput(multiline=True, readonly=True)
+        chat_scrollview.add_widget(chat_logs)
+        
+        layout.add_widget(scores_title)
+        layout.add_widget(teams_layout)
+        layout.add_widget(names_scores_layout)
+        layout.add_widget(chat_scrollview)
+    
+        ####
+
 
     def on_enter(self):
         """
@@ -70,13 +110,13 @@ class PlayerActionScreen(Screen):
 
             # Check if a green player has scored on red base
             if id_hit == common.UDP_RED_BASE_SCORED and not hitter[1]:
-                print('The green player ' + hitter[0][2] + ' has scored on Red Base!')
+                chat_logs += f'The green player ' + hitter[0][2] + ' has scored on Red Base!\n'
             # Check if a red player has scored on green base
             elif id_hit == common.UDP_GREEN_BASE_SCORED and hitter[1]:
-                print('The red player ' + hitter[0][2] + ' has scored on Green Base!')
+                chat_logs += f'The red player ' + hitter[0][2] + ' has scored on Green Base!\n'
             # Normal tag
             else:
-                print('The player ' + hitter[0][2] + ' has tagged the player ' + hittee[0][2] + '!')
+                chat_logs += f'The player ' + hitter[0][2] + ' has tagged the player ' + hittee[0][2] + '!\n'
 
             # broadcast whomever was hit
             self.laser_tag_system.udp.broadcast(id_hit)
